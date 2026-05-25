f15a3ef922cd8d9d491e2c90b2c97e7a7c237a52
Eric <erk@live.ca>
2026-05-25 01:07:41 -0400

    fix: disable etag and set cache control for API responses

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const qrcode = require('qrcode');
 
 const app = express();
 app.set('trust proxy', 1);
+app.disable('etag');
 const server = http.createServer(app);
 const port = Number(process.env.PORT || 3000);
 const dataDir = path.join(__dirname, '..', 'data');
@@ -2538,6 +2539,15 @@ if (corsAllowlist.length) {
 } else if (process.env.KEPT_CORS_ALLOW_ALL === '1') {
   app.use(cors({ origin: '*' }));
 }
+app.use('/api', (_req, res, next) => {
+  res.set({
+    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+    Pragma: 'no-cache',
+    Expires: '0',
+    'Surrogate-Control': 'no-store'
+  });
+  next();
+});
 app.use(express.json({ limit: '25mb' }));
 
 app.get('/api/setup/status', asyncRoute(async (_req, res) => {
